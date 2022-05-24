fa104921b82ad17021f2d7ba5d994d3de1fb3dba
Drake Talley <robertdraketalley@gmail.com>
2022-05-24 09:52:09 +0000

    silly-programs: update 0.1.1.beta.3 bottle.

--- Formula/silly-programs.rb
+++ Formula/silly-programs.rb
@@ -6,9 +6,9 @@ class SillyPrograms < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/Axylos/homebrew-silly-programs/releases/download/silly_programs-0.1.1.beta.3"
-    sha256 cellar: :any_skip_relocation, big_sur:      "13cb3cec17f1794d27d597a182a8a354624e0998c70c40d2a9d4fc12b1ef1ad9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "46cfd94d358b359977b30fd86da4e944136aa1440966ca23912643f6ab3c554b"
+    root_url "https://github.com/Axylos/homebrew-silly-programs/releases/download/silly-programs-0.1.1.beta.3"
+    sha256 cellar: :any_skip_relocation, big_sur:      "be2f2edc70cda7619c03e2aa55bf934e16125ca449ee0202c5d12bf8629bcd5f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "82db2fbfd597af8dd0ad3198b5b77adb0a68dbcd02fab101c41687ac2cf5499a"
   end
 
   depends_on "rust" => :build
